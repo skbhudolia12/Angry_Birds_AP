78454c131480afefece8581f1d2387d450c3564f
Sak-drago <sakshatsachdeva@gmail.com>
2024-10-21 16:58:58 +0530

    Task Started: Loading Screen Fix. Task Complete: Main Menu

--- core/src/main/java/io/github/angy/birds/screen/MainMenu.java
+++ core/src/main/java/io/github/angy/birds/screen/MainMenu.java
@@ -6,10 +6,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -24,6 +28,10 @@ public class MainMenu implements Screen {
     private Image backgroundImageMain;
     private Image TitleScreenLogo;
     private Image playButton;
+    private Image exitButton;
+    private Image pillarImage;
+    private Image pillarImage1;
+    private Image pillarImage2;
     private Texture backgroundTexture;
     private TextureRegion backgroundTextureRegion;
     private TextureRegionDrawable backgroundDrawable;
@@ -51,19 +59,53 @@ public class MainMenu implements Screen {
         skin.add("background", backgroundTextureRegion);
 
         TitleScreenLogo = new Image(new Texture(Gdx.files.internal("textures/backgrounds/titlescreentitle.png")));
-        TitleScreenLogo.setSize(400, 100);
+        TitleScreenLogo.setSize(400, 150);
         TitleScreenLogo.setPosition((stage.getWidth()-TitleScreenLogo.getWidth())/2, stage.getHeight()/2+50);
         stage.addActor(TitleScreenLogo);
         skin.add("title", TitleScreenLogo);
 
+        pillarImage = new Image(new Texture(Gdx.files.internal("ui/pillar.png")));
+        pillarImage1 = new Image(new Texture(Gdx.files.internal("ui/pillar.png")));
+        pillarImage2 = new Image(new Texture(Gdx.files.internal("ui/pillar.png")));
 
+        pillarImage.setSize(500, 500);
+        pillarImage1.setSize(500, 500);
+        pillarImage2.setSize(500, 500);
+
+        pillarImage.setPosition((stage.getWidth()-pillarImage.getWidth())/2-200, stage.getHeight()/2-300);
+        pillarImage1.setPosition((stage.getWidth()-pillarImage.getWidth())/2, stage.getHeight()/2-350);
+        pillarImage2.setPosition((stage.getWidth()-pillarImage.getWidth())/2+200, stage.getHeight()/2-250);
+
+        stage.addActor(pillarImage);
+        stage.addActor(pillarImage1);
+        stage.addActor(pillarImage2);
+
+        skin.add("pillar", pillarImage);
         // Create button style
     }
 
     private void createButtons() {
-        playButton = new Image(new Texture(Gdx.files.internal("ui/playbutton.png")));
-        playButton.setSize(100, 50);
-        playButton.setPosition((stage.getWidth()-playButton.getWidth())/2, stage.getHeight()/2-100);
+        playButton = new Image(new Texture(Gdx.files.internal("ui/playButton_bird.png")));
+        playButton.setSize(120, 120);
+        playButton.setPosition((stage.getWidth()-pillarImage.getWidth())/2-10, stage.getHeight()/2-45);
+        playButton.setZIndex(2);
+        playButton.addListener(new InputListener(){
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                ScaleToAction enlargeAction = new ScaleToAction();
+                enlargeAction.setScale(1.1f);
+                enlargeAction.setDuration(0.2f);
+                playButton.addAction(enlargeAction);
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                ScaleToAction shrinkAction = new ScaleToAction();
+                shrinkAction.setScale(1.0f);
+                shrinkAction.setDuration(0.2f);
+                playButton.addAction(shrinkAction);
+            }
+        });
+
         playButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
@@ -72,6 +114,37 @@ public class MainMenu implements Screen {
         });
         stage.addActor(playButton);
         skin.add("play", playButton);
+
+        exitButton = new Image(new Texture(Gdx.files.internal("ui/exitButton_bird.png")));
+        exitButton.setSize(120, 120);
+        exitButton.setPosition((stage.getWidth()-pillarImage.getWidth())/2+385, stage.getHeight()/2-10);
+        exitButton.setZIndex(2);
+        exitButton.addListener(new InputListener(){
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                ScaleToAction enlargeAction = new ScaleToAction();
+                enlargeAction.setScale(1.1f);
+                enlargeAction.setDuration(0.2f);
+                exitButton.addAction(enlargeAction);
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                ScaleToAction shrinkAction = new ScaleToAction();
+                shrinkAction.setScale(1.0f);
+                shrinkAction.setDuration(0.2f);
+                exitButton.addAction(shrinkAction);
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        stage.addActor(exitButton);
+        skin.add("exit", exitButton);
 
     }
 
