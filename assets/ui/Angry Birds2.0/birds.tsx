e4ad5de06e9b5371ebea7a2b8b6aff17599f40d0
skbhudolia12 <bhudoliasarthak4@gmail.com>
2024-10-22 15:06:27 +0530

    na

---
+++ assets/ui/Angry Birds2.0/birds.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="birds" tilewidth="32" tileheight="32" tilecount="756" columns="54">
+ <image source="c4caa39bd9983dd0c81cdee1efb82108.png" width="1735" height="466"/>
+</tileset>
---
+++ assets/ui/Angry Birds2.0/score.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="kisspng-angry-birds-friends-angry-birds-rio-angry-birds-2-angry-bird-5aceab583781d6.6292766615234937202274" tilewidth="32" tileheight="32" tilecount="2079" columns="33">
+ <image source="kisspng-angry-birds-friends-angry-birds-rio-angry-birds-2-angry-bird-5aceab583781d6.6292766615234937202274.png" width="1069" height="2045"/>
+</tileset>
